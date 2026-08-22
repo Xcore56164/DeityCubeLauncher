09496fbbda4724cdea40555bb5f2beb445a860df
Xcore <gaka56164@gmail.com>
2026-08-22 12:25:36 +0200

    Correction JavaFX.

---
+++ src/main/java/fr/deitycube/launcher/Main.java
@@ -0,0 +1,13 @@
+package fr.deitycube.launcher;
+
+import fr.deitycube.launcher.ui.LauncherApp;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        LauncherApp.main(args);
+    }
+}
